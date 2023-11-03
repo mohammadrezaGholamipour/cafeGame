543c288ece278c2d4af63f0f2a8100117b14a577
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2023-11-04 00:41:56 +0330

    custom and set beutify style for datePicker component

--- src/views/bill/components/report.vue
+++ src/views/bill/components/report.vue
@@ -17,29 +17,43 @@ watch(
     jalaliDate.value = new JalaliDate(new Date(value ? value : "")).format(
       "dddd DD MMMM YYYY"
     );
-  }
+  },
+  { deep: true }
 );
 </script>
 <template>
   <div class="parent-report">
     <!-- ////////////////////////////// -->
-    <div>
+    <div class="!p-0">
       <p>تاریخ :</p>
       <date-picker
         @close="dateStatus = false"
         format="YYYY-MM-DD"
         v-model="inputDate"
         :show="dateStatus"
-        id="date-picker"
         simple
       />
-      <p @click="dateStatus = !dateStatus">{{ jalaliDate }}</p>
+      <button
+        :disabled="false"
+        :class="[
+          'button bg-[#3ea6da] p-2 text-white',
+          { 'bg-gray-400 !cursor-not-allowed': false },
+        ]"
+        @click="dateStatus = !dateStatus"
+      >
+        <transition-fade class="flex" group>
+          <span v-if="!jalaliDate" class="btn-loader"></span>
+          <p v-else>{{ jalaliDate }}</p>
+        </transition-fade>
+      </button>
     </div>
     <!-- ////////////////////////////// -->
     <div>
       <p>هزینه بازی شده :</p>
       <p>تومان</p>
     </div>
+    <!-- ////////////////////////////// -->
+    <div class="w-full border !p-0 border-solid border-slate-500"></div>
     <!-- ////////////////////////////// -->
     <div>
       <p>خوراکی های فروخته شده :</p>
@@ -55,12 +69,39 @@ watch(
 </template>
 <style>
 .parent-report {
-  @apply w-full flex flex-col justify-center items-center gap-y-[10px];
+  @apply w-full p-2 flex flex-col justify-center items-center;
 }
 .parent-report > div {
-  @apply w-full flex justify-between items-center;
+  @apply w-full flex justify-between items-center p-2.5;
 }
-#date-picker .vpd-input-group {
+.vpd-input-group {
   display: none;
+}
+.vpd-body {
+  @apply p-2.5;
+}
+.vpd-container {
+  @apply rounded-md overflow-hidden;
+}
+.vpd-actions {
+  @apply text-center mt-2;
+}
+.vpd-simple-content .vpd-column {
+  @apply gap-y-[10px];
+}
+.vpd-simple-content .vpd-column .vpd-column-content:after,
+.vpd-simple-content .vpd-column .vpd-column-content:before {
+  @apply !hidden;
+}
+.parent-report > div:nth-child(2) {
+  @apply mt-3  rounded-t-md;
+  background: linear-gradient(92deg, #f8b806 -30.82%, #ff8c04 126.36%);
+}
+.parent-report > div:nth-child(4) {
+  background: linear-gradient(92deg, #f8b806 -30.82%, #ff8c04 126.36%);
+}
+.parent-report > div:nth-child(5) {
+  @apply rounded-b-md;
+  background: linear-gradient(95deg, #32bb71 15.3%, #2a9d8f 113.45%);
 }
 </style>

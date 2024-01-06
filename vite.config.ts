1a4ced8da6e1589fc001eea15021ddc22adea201
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2024-01-06 12:59:57 +0330

    set pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,36 @@
 import { fileURLToPath, URL } from "node:url";
-
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "My Awesome App",
+        short_name: "AwesomeApp",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        // ...other manifest properties
+      },
+      // ...other PWA plugin options
+    }),
+    vue(),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),

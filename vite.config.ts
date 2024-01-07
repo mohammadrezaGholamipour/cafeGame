7a1326cac76baf71ac96c69553593ce2b4f874da
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2024-01-07 16:05:37 +0330

    set pwa for project

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,32 @@
 import { fileURLToPath, URL } from "node:url";
 import { VitePWA } from "vite-plugin-pwa";
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+//////////////////////////////////
 export default defineConfig({
   plugins: [
+    vue(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
       manifest: {
-        name: "My Awesome App",
-        short_name: "AwesomeApp",
-        theme_color: "#ffffff",
+        name: "CafeGame",
+        short_name: "Cafe",
+        display: "standalone",
+        description: "Console and cafe food management",
+        background_color: "#1f5b77",
+        theme_color: "#1f5b77",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "/public/icon.png",
             sizes: "192x192",
             type: "image/png",
           },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
         ],
-        // ...other manifest properties
       },
-      // ...other PWA plugin options
+      devOptions: {
+        enabled: true,
+      },
     }),
-    vue(),
   ],
   resolve: {
     alias: {
